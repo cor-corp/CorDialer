0195a0dc79325f1601b941e8cf0713bd22431519
anton.bakaliar <anton.bakaliar@flexaspect.com>
2020-06-23 18:13:29 +0300

    addcontacts

--- app/src/main/java/com/corcorp/cordialer/Contacts.java
+++ app/src/main/java/com/corcorp/cordialer/Contacts.java
@@ -3,19 +3,29 @@ package com.corcorp.cordialer;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Contacts extends AppCompatActivity  {
+    public TextView contacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+        contacts = (TextView) findViewById(R.id.contact);
+
+        getContacts();
+
 
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -49,6 +59,56 @@ public class Contacts extends AppCompatActivity  {
     public void clickDialer(View view) {
         Intent intent = new Intent(Contacts.this, Dialer.class);
         startActivity(intent);
+
+
+    }
+
+    public void getContacts(){
+        String phoneNumber = null;
+
+        //Связываемся с контактными данными и берем с них значения id контакта, имени контакта и его номера:
+        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+        String _ID = ContactsContract.Contacts._ID;
+        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+
+        Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+
+        StringBuffer output = new StringBuffer();
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(CONTENT_URI, null,null, null, null);
+
+        //Запускаем цикл обработчик для каждого контакта:
+        if (cursor.getCount() > 0) {
+
+            //Если значение имени и номера контакта больше 0 (то есть они существуют) выбираем
+            //их значения в приложение привязываем с соответствующие поля "Имя" и "Номер":
+            while (cursor.moveToNext()) {
+                String contact_id = cursor.getString(cursor.getColumnIndex(_ID));
+                String name = cursor.getString(cursor.getColumnIndex(DISPLAY_NAME));
+                int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex(HAS_PHONE_NUMBER)));
+
+                //Получаем имя:
+                if (hasPhoneNumber > 0) {
+                    output.append("\n Имя: " + name);
+                    Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null,
+                            Phone_CONTACT_ID + " = ?", new String[]{contact_id}, null);
+
+                    //и соответствующий ему номер:
+                    while (phoneCursor.moveToNext()) {
+                        phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
+                        output.append("\n Телефон: " + phoneNumber);
+                    }
+                }
+                output.append("\n");
+            }
+
+            //Полученные данные отображаем с созданном элементе TextView:
+            contacts.setText(output);
+        }
+
 
 
     }
